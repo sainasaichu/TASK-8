4870facbb90aa2867baa6d2d3eb1cda7e765ca27
Sahina Begaum <149920611+sainasaichu@users.noreply.github.com>
2023-12-14 11:19:42 +0530

    Add files via upload

---
+++ Task 1 oops.py
@@ -0,0 +1,9 @@
+#The list is [10, 501, 22, 37, 100, 999, 87, 351]
+class circle:
+    def __init__(self):
+    self.num= [10, 501, 22, 37, 100, 999, 87, 351]
+    def read_number(self):
+        print(self.num)
+        obj = circle()
+        obj.read_number()
+
---
+++ Task 2 oops.py
@@ -0,0 +1,5 @@
+class myClass:
+    a = 33;
+    def __privmeth(self):
+        print(""
+              "")
